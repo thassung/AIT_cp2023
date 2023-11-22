92af2d2d71723d3663fbed7055472f3dd70b5332
thassung <105700459+thassung@users.noreply.github.com>
2023-11-23 00:11:40 +0700

    add EDA

--- app/main.py
+++ app/main.py
@@ -79,4 +79,4 @@ def predict():
     return jsonify(result), 200
         
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',port='8000',debug=True)
+    app.run(host='127.0.0.1',port='8080',debug=False)
